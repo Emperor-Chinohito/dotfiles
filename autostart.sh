1706e839847fcbe141f5834a74f7d4e909f49e33
pj <73098012+Emperor-Chinohito@users.noreply.github.com>
2022-10-28 01:13:48 -0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,6 @@
 # This is my dwm autostart file
 # I have patched dwm with autostart.diff but you could just add this to .xinitrc
 
-xcompmgr &                                              # Compositor for transparency
-feh --bg-scale ~/dwm-orange/Wallpapers/Porco_Rosso.jpg  # Wallpaper
-./.dotfiles/dwmbar &                                    # Statusbar script
+xcompmgr &                                                # Compositor for transparency
+feh --bg-scale ~/dwm-orange/Wallpapers/Porco_Rosso.jpg    # Wallpaper (alternatively you could do ./fehbg)
+./.dotfiles/dwmbar &                                      # Statusbar script (remember to set the correct path to this script)
